9d585368cb15aded9f97dabeb1aed22be1a37cad
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2018-08-23 22:46:03 +0100

    implement dynamic menu rendering

---
+++ ui/js/nav.js
@@ -0,0 +1,10 @@
+const menuIcon = document.getElementById('menu-icon');
+const menu = document.getElementsByClassName('header');
+
+menuIcon.addEventListener('click', () => {
+    if (menu[0].style.display === 'flex') {
+        menu[0].style.display = 'none';
+    } else {
+        menu[0].style.display = 'flex';
+    }
+});
