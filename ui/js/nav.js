57e5c0970efda36a700abb840b3f54d64a6149dd
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2018-08-28 23:22:19 +0100

    adjust navbar and set up eslint

--- ui/js/nav.js
+++ ui/js/nav.js
@@ -2,9 +2,9 @@ const menuIcon = document.getElementById('menu-icon');
 const menu = document.getElementsByClassName('header');
 
 menuIcon.addEventListener('click', () => {
-    if (menu[0].style.display === 'flex') {
-        menu[0].style.display = 'none';
-    } else {
-        menu[0].style.display = 'flex';
-    }
+  if (menu[0].style.display === 'flex') {
+    menu[0].style.display = 'none';
+  } else {
+    menu[0].style.display = 'flex';
+  }
 });
