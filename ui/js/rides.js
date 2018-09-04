d6426a1d32686b954d34606db2b7ecf90508f6f4
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2018-09-04 22:29:09 +0100

    fetch available rides from api

---
+++ ui/js/rides.js
@@ -0,0 +1,32 @@
+const ridesContainer = document.getElementsByClassName('container')[0];
+let htmlContent = '';
+
+fetch('https://shrouded-plains-80012.herokuapp.com/api/v1/rides', {
+  method: 'GET',
+  headers: {
+    'Content-type': 'application/json',
+    token: localStorage.rideMyWayToken,
+  },
+}).then(response => response.json()).then((data) => {
+  if (data.success) {
+    for (let i = 0; i < data.body.length; i++) {
+      htmlContent = `<div class="ride">
+                      <div class="ride-body">
+                        <div class="ride-header">
+                            <h3>${data.body[i].location} - ${data.body[i].destination}</h3>
+                        </div>
+                        <p><strong>Date: </strong>${new Date(data.body[i].date).toDateString()}</p>
+                        <p><strong>Driver: </strong>${data.body[i].driver}</p>
+                        <p><strong>Location: </strong>${data.body[i].location}</p>
+                        <p><strong>Destination: </strong>${data.body[i].destination}</p>
+                        <p><strong>Departure Time: </strong>${data.body[i].departuretime}</p>
+                        <button id="request-ride" class="submit request">Request Ride</button>
+                        </div></div>`;
+      ridesContainer.insertAdjacentHTML('beforeend', htmlContent);
+    }
+  } else {
+    ridesContainer.innerHTML = `<p>${data.message}</p>`;
+  }
+}).catch((err) => {
+  ridesContainer.innerHTML = `<p>${err}</p>`;
+});
