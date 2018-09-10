a7a9f6ee2315cba9f59f27a74a5b9abc303cb0d4
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2018-09-10 22:30:03 +0100

    display rides taken by user in profile page

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # ride-my-way
 This is a carpooling application that allows users to offer rides to others and to request for rides from others as well.
 
+## Functionality
+The Server Side functionality of this app is housed in its API [here](https://github.com/elikeyz/ride-my-way). 
+
 ## Usage
 To start, simply open the index.html file. 
--- ui/js/profile.js
+++ ui/js/profile.js
@@ -29,12 +29,48 @@ fetch('https://shrouded-plains-80012.herokuapp.com/api/v1/rides', {
         container.insertAdjacentHTML('beforeend', htmlContent);
       }
     }
-    const htmlContent = `<h3>${localStorage.rideMyWayUserFirstName} ${localStorage.rideMyWayUserLastName}</h3>
+    fetch('https://shrouded-plains-80012.herokuapp.com/api/v1/users/requests', {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+        token: localStorage.rideMyWayToken,
+      },
+    }).then(response => response.json()).then((requests) => {
+      if (requests.success) {
+        for (let j = 0; j < requests.body.length; j += 1) {
+          fetch(`https://shrouded-plains-80012.herokuapp.com/api/v1/rides/${requests.body[j].rideid}`, {
+            method: 'GET',
+            headers: {
+              'Content-type': 'application/json',
+              token: localStorage.rideMyWayToken,
+            },
+          }).then(response => response.json()).then((ride) => {
+            if (ride.success) {
+              const htmlContent = `<div class="ride">                    
+                                <div class="ride-body">
+                                  <div class="ride-header">
+                                    <h3>${ride.body.location} - ${ride.body.destination}</h3>
+                                  </div>
+                                  <p><strong>Date: </strong>${new Date(ride.body.date).toDateString()}</p>
+                                  <p><strong>Driver: </strong>${ride.body.driver}</p>
+                                  <p><strong>Location: </strong>${ride.body.location}</p>
+                                  <p><strong>Destination: </strong>${ride.body.destination}</p>
+                                  <p><strong>Departure Time: </strong>${ride.body.departuretime}</p>
+                                  <p><strong>Status: </strong>Taken</p>
+                                </div>
+                              </div>`;
+              container.insertAdjacentHTML('beforeend', htmlContent);
+            }
+          });
+        }
+        const htmlContent = `<h3>${localStorage.rideMyWayUserFirstName} ${localStorage.rideMyWayUserLastName}</h3>
                     <h4>${localStorage.rideMyWayUserEmail}</h4>
                     <h4>${localStorage.rideMyWayUserUserName}</h4>
                     <h4>Number of Rides Given: ${givenRidesCount}</h4>
-                    <h4>Number of Rides Taken: 0</h4>`;
+                    <h4>Number of Rides Taken: ${requests.body.length}</h4>`;
 
-    userInfo.innerHTML = htmlContent;
+        userInfo.innerHTML = htmlContent;
+      }
+    });
   }
 });
