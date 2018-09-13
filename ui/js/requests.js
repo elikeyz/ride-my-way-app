e436fe4f664e75723457b8e38894d37d3d9dcfdd
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2018-09-13 22:20:13 +0100

    add event listeners to respond to ride requests

--- ui/js/requests.js
+++ ui/js/requests.js
@@ -3,6 +3,8 @@ if (!localStorage.rideMyWayToken) {
 }
 
 const container = document.getElementsByClassName('container')[0];
+const modal = document.getElementById('myModal');
+const modalBody = document.getElementsByClassName('modal-body')[0];
 const myRides = [];
 
 fetch('https://shrouded-plains-80012.herokuapp.com/api/v1/rides', {
@@ -25,26 +27,26 @@ fetch('https://shrouded-plains-80012.herokuapp.com/api/v1/rides', {
           'Content-type': 'application/json',
           token: localStorage.rideMyWayToken,
         },
-      }).then(response => response.json()).then((request) => {
-        if (request.success && request.body.length > 0) {
-          for (let k = 0; k < request.body.length; k += 1) {
+      }).then(response => response.json()).then((requests) => {
+        if (requests.success && requests.body.length > 0) {
+          for (let k = 0; k < requests.body.length; k += 1) {
             let subContent = '';
             if (new Date(myRides[j].date) <= new Date()) {
               subContent = '<p><em>Expired</em></p>';
-            } else if (request.body[k].isaccepted) {
-              subContent = '<p><em>Accepted</em></p> <button id="reject-req" class="submit action">Reject</button>';
-            } else if (request.body[k].isaccepted === false) {
-              subContent = '<button id="accept-req" class="submit action">Accept</button> <p><em>Rejected</em></p>';
-            } else if (request.body[k].isaccepted === null) {
-              subContent = '<button id="accept-req" class="submit action">Accept</button><button id="reject-req" class="submit action">Reject</button>';
+            } else if (requests.body[k].isaccepted) {
+              subContent = `<p><em>Accepted</em></p> <button id="reject-req${requests.body[k].id}" class="submit action">Reject</button>`;
+            } else if (requests.body[k].isaccepted === false) {
+              subContent = `<button id="accept-req${requests.body[k].id}" class="submit action">Accept</button> <p><em>Rejected</em></p>`;
+            } else if (requests.body[k].isaccepted === null) {
+              subContent = `<button id="accept-req${requests.body[k].id}" class="submit action">Accept</button><button id="reject-req${requests.body[k].id}" class="submit action">Reject</button>`;
             }
             const htmlContent = `<div class="ride">            
                                  <div class="ride-body">
                                    <div class="ride-header">
-                                     <h3>${request.body[k].passenger}: ${myRides[j].location} - ${myRides[j].destination}</h3>
+                                     <h3>${requests.body[k].passenger}: ${myRides[j].location} - ${myRides[j].destination}</h3>
                                    </div>
                                    <p><strong>Date: </strong>${new Date(myRides[j].date).toDateString()}</p>
-                                   <p><strong>Passenger: </strong>${request.body[k].passenger}</p>
+                                   <p><strong>Passenger: </strong>${requests.body[k].passenger}</p>
                                    <p><strong>Location: </strong>${myRides[j].location}</p>
                                    <p><strong>Destination: </strong>${myRides[j].destination}</p>
                                    <p><strong>Departure Time: </strong>${myRides[j].departuretime}</p>
@@ -54,7 +56,92 @@ fetch('https://shrouded-plains-80012.herokuapp.com/api/v1/rides', {
             container.insertAdjacentHTML('beforeend', htmlContent);
           }
         }
-        return request;
+        return requests;
+      }).then((requests) => {
+        if (requests.success && requests.body.length > 0) {
+          for (let k = 0; k < requests.body.length; k += 1) {
+            if (new Date(myRides[j].date) >= new Date()) {
+              const acceptBtn = document.getElementById(`accept-req${requests.body[k].id}`);
+              const rejectBtn = document.getElementById(`reject-req${requests.body[k].id}`);
+              if (acceptBtn) {
+                acceptBtn.addEventListener('click', (rideCopy => (requestCopy => () => {
+                  fetch(`https://shrouded-plains-80012.herokuapp.com/api/v1/users/rides/${rideCopy.id}/requests/${requestCopy.id}`, {
+                    method: 'PUT',
+                    headers: {
+                      'Content-type': 'application/json',
+                      token: localStorage.rideMyWayToken,
+                    },
+                    body: JSON.stringify({
+                      accept: 'true',
+                    }),
+                  }).then(response => response.json()).then((accept) => {
+                    let feedback = '';
+                    if (accept.success) {
+                      feedback = `<p>
+                                  Request from ${requestCopy.passenger}<br>
+                                  ${rideCopy.location} - ${rideCopy.destination}<br>
+                                  ${new Date(rideCopy.date)}<br>
+                                  ${rideCopy.departuretime}<br>
+                                  ${accept.message}<br>
+                                  <button class="submit modal-btn">Ok</button>
+                                </p>`;
+                    } else {
+                      feedback = `<p>
+                                  ${accept.message}<br>
+                                  <button class="submit modal-btn">Ok</button>
+                                </p>`;
+                    }
+                    modal.style.display = 'block';
+                    modalBody.innerHTML = feedback;
+                    const modalBtn = document.getElementsByClassName('modal-btn')[0];
+                    modalBtn.addEventListener('click', () => {
+                      modal.style.display = 'none';
+                      window.location = '../ui/requests.html';
+                    });
+                  });
+                })(requests.body[k]))(myRides[j]));
+              }
+              if (rejectBtn) {
+                rejectBtn.addEventListener('click', (rideCopy => (requestCopy => () => {
+                  fetch(`https://shrouded-plains-80012.herokuapp.com/api/v1/users/rides/${rideCopy.id}/requests/${requestCopy.id}`, {
+                    method: 'PUT',
+                    headers: {
+                      'Content-type': 'application/json',
+                      token: localStorage.rideMyWayToken,
+                    },
+                    body: JSON.stringify({
+                      accept: 'false',
+                    }),
+                  }).then(response => response.json()).then((reject) => {
+                    let feedback = '';
+                    if (reject.success) {
+                      feedback = `<p>
+                                Request from ${requestCopy.passenger}<br>
+                                ${rideCopy.location} - ${rideCopy.destination}<br>
+                                ${new Date(rideCopy.date)}<br>
+                                ${rideCopy.departuretime}<br>
+                                ${reject.message}<br>
+                                <button class="submit modal-btn">Ok</button>
+                              </p>`;
+                    } else {
+                      feedback = `<p>
+                                ${reject.message}<br>
+                                <button class="submit modal-btn">Ok</button>
+                              </p>`;
+                    }
+                    modal.style.display = 'block';
+                    modalBody.innerHTML = feedback;
+                    const modalBtn = document.getElementsByClassName('modal-btn')[0];
+                    modalBtn.addEventListener('click', () => {
+                      modal.style.display = 'none';
+                      window.location = '../ui/requests.html';
+                    });
+                  });
+                })(requests.body[k]))(myRides[j]));
+              }
+            }
+          }
+        }
       });
     }
   }
--- ui/js/rides.js
+++ ui/js/rides.js
@@ -68,41 +68,44 @@ fetch('https://shrouded-plains-80012.herokuapp.com/api/v1/users/requests', {
     }
     return data;
   }).then((data) => {
-    for (let i = 0; i < data.body.length; i += 1) {
-      if (new Date(data.body[i].date) >= new Date()) {
-        const requestBtn = document.getElementById(`request-ride${data.body[i].id}`);
-        if (data.body[i].driver !== localStorage.rideMyWayUserUserName) {
-          requestBtn.addEventListener('click', (rideCopy => () => {
-            fetch(`https://shrouded-plains-80012.herokuapp.com/api/v1/rides/${rideCopy.id}/requests`, {
-              method: 'POST',
-              headers: {
-                'Content-type': 'application/json',
-                token: localStorage.rideMyWayToken,
-              },
-            }).then(response => response.json()).then((request) => {
-              let feedback = '';
-              if (request.success) {
-                feedback = `<p>
-                                ${rideCopy.location} - ${rideCopy.destination}<br>
-                                ${new Date(rideCopy.date)}<br>
-                                ${rideCopy.departuretime}<br>
-                                ${request.message}<br>
-                                <button class="submit modal-btn">Ok</button>
-                              </p>`;
-              } else {
-                feedback = `<p>
-                                ${request.message}<br>
-                                <button class="submit modal-btn">Ok</button>
-                              </p>`;
-              }
-              modal.style.display = 'block';
-              modalBody.innerHTML = feedback;
-              const modalBtn = document.getElementsByClassName('modal-btn')[0];
-              modalBtn.addEventListener('click', () => {
-                modal.style.display = 'none';
+    if (data.success) {
+      for (let i = 0; i < data.body.length; i += 1) {
+        if (new Date(data.body[i].date) >= new Date()) {
+          const requestBtn = document.getElementById(`request-ride${data.body[i].id}`);
+          if (data.body[i].driver !== localStorage.rideMyWayUserUserName) {
+            requestBtn.addEventListener('click', (rideCopy => () => {
+              fetch(`https://shrouded-plains-80012.herokuapp.com/api/v1/rides/${rideCopy.id}/requests`, {
+                method: 'POST',
+                headers: {
+                  'Content-type': 'application/json',
+                  token: localStorage.rideMyWayToken,
+                },
+              }).then(response => response.json()).then((request) => {
+                let feedback = '';
+                if (request.success) {
+                  feedback = `<p>
+                                  ${rideCopy.location} - ${rideCopy.destination}<br>
+                                  ${new Date(rideCopy.date)}<br>
+                                  ${rideCopy.departuretime}<br>
+                                  ${request.message}<br>
+                                  <button class="submit modal-btn">Ok</button>
+                                </p>`;
+                } else {
+                  feedback = `<p>
+                                  ${request.message}<br>
+                                  <button class="submit modal-btn">Ok</button>
+                                </p>`;
+                }
+                modal.style.display = 'block';
+                modalBody.innerHTML = feedback;
+                const modalBtn = document.getElementsByClassName('modal-btn')[0];
+                modalBtn.addEventListener('click', () => {
+                  modal.style.display = 'none';
+                  window.location = '../ui/rides.html';
+                });
               });
-            });
-          })(data.body[i]));
+            })(data.body[i]));
+          }
         }
       }
     }
